995748eda86e056a8ccf530e5c9699dddde0ccc3
eunaotenhocriatividade <andreicruz1203@gmail.com>
2025-03-13 20:31:09 -0300

    Update main.jsx

--- Margaridas/src/main.jsx
+++ Margaridas/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from 'componentes/App.jsx'
+import App from './componentes/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  
     <App />
   </StrictMode>,
 )
